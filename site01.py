c8d4f80449efd69be8516d40ea36c8cf6d144c53
class3Sti <studiesdragon5@gmail.com>
2025-10-30 11:11:16 +0000

    Adicionando funcionalidade no botão

--- site01.py
+++ site01.py
@@ -1,10 +1,11 @@
-# Site com instruções de como usar o Streamlit
-# https://docs.streamlit.io/develop/concepts
+# Instruções de como usar os elementos do Streamlit
+# https://docs.streamlit.io/develop/api-reference
 
 
 import streamlit as st
 
 if __name__ == '__main__':
     st.write('*E aí,* **CE Valadão**!')
-    st.button('OK', on_click='')
+    st.button('OK', on_click=lambda: st.write('Clicou no botão!'))
     st.write('E aí, **Adan**!')
+
