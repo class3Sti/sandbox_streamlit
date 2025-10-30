2f8abc68efd38a6ffd12d2f4ce80734d74a3907d
class3Sti <studiesdragon5@gmail.com>
2025-10-30 16:15:10 +0000

    linkando com o Smartsampa

--- site01.py
+++ site01.py
@@ -9,6 +9,11 @@ if __name__ == '__main__':
     st.button('OK', on_click=lambda: st.write('Clicou no botão!'))
     st.write('E aí, **Adan**!')
     txt = st.text_area('Escreva seu texto aqui')
-    prompt = st.chat_input('Diga alguma coisa')
-    if prompt:
-        st.write(f'Você disse: {prompt}')
+
+    # Chat individual
+    # prompt = st.chat_input('Diga alguma coisa')
+    # if prompt:
+    #     st.write(f'Você disse: {prompt}')
+
+    st.page_link(page='smartsampa.sentinelx.com.br', label='Acesse o SmartSampa')
+    
