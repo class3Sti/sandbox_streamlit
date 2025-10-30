10b9377c28819c2322e4fe3b42adce920e39d9af
class3Sti <studiesdragon5@gmail.com>
2025-10-30 10:16:17 +0000

    Linkando nosso site

--- site01.py
+++ site01.py
@@ -6,4 +6,5 @@ import streamlit as st
 
 if __name__ == '__main__':
     st.write('*E aí,* **CE Valadão**!')
-    st.button('OK')
+    st.button('OK', on_click='')
+    st.page_link('Ir para a página de teste', page_name='teste.html')
