1348e35325cc4eef14de3ff4aa43030497e7f7d8
class3Sti <studiesdragon5@gmail.com>
2025-10-30 16:28:54 +0000

    Adicionando menu lateral esquerdo

--- site01.py
+++ site01.py
@@ -16,3 +16,15 @@ if __name__ == '__main__':
     #     st.write(f'Você disse: {prompt}')
 
     st.page_link(page='https://smartsampa.sentinelx.com.br', label='Acesse o SmartSampa')
+
+
+    # Definindo um menu lateral esquerdo
+    paginas = {
+        'Dados': [
+            st.Page('hello_world.py', title='Olá mundo'),
+            st.Page('hw.py', title='Olá mundo reduzido'),
+        ],
+    }
+
+    menu_lateral_esquerdo = st.navigation(paginas)
+    menu_lateral_esquerdo.run()
