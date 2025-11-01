b1d493b1d0331febdf9044ae698115cd52e320ab
tps <Wellington.Lima@smsussp.int>
2025-11-01 17:47:57 -0300

    Organização básica do menu lateral esquerdo

--- site01.py
+++ site01.py
@@ -1,12 +1,22 @@
 # Instruções de como usar os elementos do Streamlit
 # https://docs.streamlit.io/develop/api-reference
 
+import streamlit as st
+
 def pagina_auxiliar():
     st.title('Page 2')
 
-import streamlit as st
+def p3():
+    st.title('Página 3.')
 
-if __name__ == '__main__':
+def pagina_referencia01():
+    st.title('Primeira página de referência')
+
+def home_page():
+    st.title('Página Inicial')
+
+def pagina_teste():
+    st.title('Teste')
     st.write('*E aí,* **CE Valadão**!')
     st.button('OK', on_click=lambda: st.write('Clicou no botão!'))
     st.write('E aí, **Adan**!')
@@ -20,13 +30,32 @@ if __name__ == '__main__':
     st.page_link(page='https://smartsampa.sentinelx.com.br', label='Acesse o SmartSampa')
 
 
+
+if __name__ == '__main__':
     # Definindo um menu lateral esquerdo
     paginas = {
+        '': [
+            st.Page(home_page, title='Início', icon=':material/home:'),
+        ],
         'Dados': [
-            # st.Page('hello_world.py', title='Olá mundo'),
-            st.Page('hw.py', title='Olá mundo reduzido'),
+            st.Page('hw.py', title='Olá mundo'),
+            st.Page(p3, title='Olá mundo reduzido'),
+        ],
+        'Referências': [
+            st.Page(pagina_referencia01, title='referência 01'),
+        ],
+        'Teste': [
+            st.Page(pagina_teste, title='1º Teste')
         ],
     }
 
-    menu_lateral_esquerdo = st.navigation(['hw.py', pagina_auxiliar])
+    inicio = st.Page(
+        p3,
+        title='Início',
+        icon=':material/home:'
+    )
+
+    #menu_lateral_esquerdo = st.navigation(['hw.py', pagina_auxiliar, p3])
+    #menu_lateral_esquerdo = st.navigation({'Dados':[pagina_auxiliar, p3]})
+    menu_lateral_esquerdo = st.navigation(paginas)
     menu_lateral_esquerdo.run()
